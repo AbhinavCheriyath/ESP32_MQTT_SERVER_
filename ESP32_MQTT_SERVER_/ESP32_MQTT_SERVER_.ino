b9e4ae15555debb70d1cc91f70df83b775a2d1cb
Abhinav Cheriyath <abhinavcheriyath2@gmail.com>
2025-09-02 16:53:55 +0530

    Add files via upload

---
+++ ESP32_MQTT_SERVER_/ESP32_MQTT_SERVER_.ino
@@ -0,0 +1,70 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi credentials
+const char* ssid = "GRAND HOME-4G";
+const char* password = "GRANDHOME@786";
+
+// MQTT Broker details
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "esp32s3/test";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Callback when message arrives
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32S3Client")) {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" → retrying in 5s");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  if(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 5000) {
+    lastMsg = millis();
+    String msg = "Hello from ESP32-S3: " + String(millis() / 1000) + "s";
+    client.publish(mqtt_topic, msg.c_str());
+    Serial.println("Published: " + msg);
+  }
+}
+
